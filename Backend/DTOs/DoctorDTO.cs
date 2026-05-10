5d0d56c3f908547038f83b2435dfb496eb7cd5f5
Yosmel Chiang <yosmelchiang@gmail.com>
2026-05-10 08:42:55 +0200

    feat: create DTO's

---
+++ Backend/DTOs/DoctorDTO.cs
@@ -0,0 +1,11 @@
+namespace DTOS;
+
+public class DoctorDTO
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string? Email { get; set; }
+    public string? Address { get; set; }
+}
---
+++ Backend/DTOs/SpecialtyDTO.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DTOS;
+
+public class SpecialtyDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+}
+
+public class SpecialtyWithDetailsDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+
+    public IEnumerable<DoctorDTO>? Doctors { get; set; }
+}
+
+public class CreateSpecialtyDTO {
+    [Required]
+    [MinLength(2)]
+    [MaxLength(100)]
+    public string Name { get; set; } = null!;
+}
+
+public class UpdateSpecialtyDTO
+{
+    [Required]
+    [MinLength(2)]
+    [MaxLength(100)]
+    public string Name { get; set;} = null!;
+}
