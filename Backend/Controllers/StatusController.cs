fadb7df11125c83dcbb3a78cb63a9dc73016287b
Yosmel Chiang <yosmelchiang@gmail.com>
2026-05-10 11:53:55 +0200

    feat: implement Status feature, with DTOs, Service, DI Injection, Controller and Swagger documentation

--- Backend/Controllers/StatusController.cs
+++ Backend/Controllers/StatusController.cs
@@ -12,7 +12,12 @@ public class StatusController : ControllerBase
 
     public StatusController(StatusService service) => (_service) = (service); 
 
+    /// <summary>
+    /// Returns a list of statuses
+    /// </summary>
+    /// <response code="200">Resources returned</response>
     [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<StatusWithDetailsDTO>), StatusCodes.Status200OK)]
     public async Task<ActionResult<StatusWithDetailsDTO>> Get()
     {
         var statuses = await _service.GetStatuses();
@@ -20,7 +25,15 @@ public class StatusController : ControllerBase
         return Ok(statuses);
     }
 
+    /// <summary>
+    /// Returns a single status
+    /// </summary>
+    /// <param name="id"></param>
+    /// <response code="200">Resource returned</response>
+    /// <response code="404">Resource not found</response>
     [HttpGet("{id}")]
+    [ProducesResponseType(typeof(StatusWithDetailsDTO), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult<StatusDTO>> Get(int id)
     {
         var status = await _service.GetStatus(id);
@@ -29,7 +42,13 @@ public class StatusController : ControllerBase
         return Ok(status);
     }
 
+    /// <summary>
+    /// Returns a list of Appojntments for Status
+    /// </summary>
+    /// <param name="id"></param>
+    /// <response code="200">Resources returned</response>
     [HttpGet("{id}/appointments")]
+    [ProducesResponseType(typeof(IEnumerable<StatusDTO>), StatusCodes.Status200OK)]
     public async Task<ActionResult<string>> GetAppointments(int id)
     {
         var appointments = await _service.GetAppointments(id);
@@ -37,7 +56,19 @@ public class StatusController : ControllerBase
         return Ok(appointments);
     }
 
+    /// <summary>Create a new status</summary>
+    /// <remarks>
+    /// Sample request:
+    ///
+    ///     {
+    ///         "Name": "Scheduled",
+    ///     }
+    ///
+    /// </remarks>
+    /// <param name="status"></param>
+    /// <response code="201">Resource created</response>
     [HttpPost]
+    [ProducesResponseType(typeof(StatusDTO), StatusCodes.Status201Created)]
     public async Task<ActionResult<string>> Create(CreateStatusDTO status)
     {
         var result = await _service.CreateStatus(status);
@@ -51,7 +82,22 @@ public class StatusController : ControllerBase
         return CreatedAtAction(nameof(Get), new { id = created.Id}, created);
     }
 
+    /// <summary>Update a status</summary>
+    /// <remarks>
+    /// Sample request:
+    ///
+    ///     {
+    ///         "Name": "Scheduled",
+    ///     }
+    ///
+    /// </remarks>
+    /// <param name="id"></param>
+    /// <param name="status"></param>
+    /// <response code="204">Update successful, no content returned</response>
+    /// <response code="404">Resource not found by id</response>
     [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult<string>> Update(int id, UpdateStatusDTO status)
     {
         var updated = await _service.UpdateStatus(id, status);
@@ -60,7 +106,13 @@ public class StatusController : ControllerBase
         return NoContent();
     }
 
+    /// <summary>Delete a status</summary>
+    /// <param name="id"></param>
+    /// <response code="204">Deletion successful, no content returned</response>
+    /// <response code="404">Resource not found by id</response>
     [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult<string>> Delete(int id)
     {
         var deleted = await _service.DeleteStatus(id);
