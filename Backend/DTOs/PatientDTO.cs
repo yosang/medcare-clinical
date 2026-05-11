9b542e26424508ce5da23068cb6d3a37a2300f4a
Yosmel Chiang <yosmelchiang@gmail.com>
2026-05-11 13:24:01 +0200

    chore: Create DTOS and Mapper

--- Backend/DTOs/PatientDTO.cs
+++ Backend/DTOs/PatientDTO.cs
@@ -11,3 +11,39 @@ public class PatientDTO
     public int? NationalIdentityNumber { get; set; }
     public bool IsRegistered { get; set; }
 }
+
+public class PatientWithDetailsDTO
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string? Phone { get; set; }
+    public string? Email { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+    public int? NationalIdentityNumber { get; set; }
+    public bool IsRegistered { get; set; }
+
+    public IEnumerable<AppointmentDTO>? Appointments { get; set; }
+}
+
+public class CreatePatientDTO
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string? Phone { get; set; }
+    public string? Email { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+    public int? NationalIdentityNumber { get; set; }
+    public bool IsRegistered { get; set; }
+}
+
+public class UpdatePatientDTO
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string? Phone { get; set; }
+    public string? Email { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+    public int? NationalIdentityNumber { get; set; }
+    public bool IsRegistered { get; set; }
+}
--- Backend/Extensions/Mappers/PatientMapper.cs
+++ Backend/Extensions/Mappers/PatientMapper.cs
@@ -9,14 +9,45 @@ public static class PatientMapper
     {
         return new PatientDTO
         {
-          Id = patient.Id,
-          FirstName = patient.FirstName,
-          LastName = patient.LastName,
-          Phone = patient.Phone,
-          Email = patient.Email,
-          DateOfBirth = patient.DateOfBirth,
-          NationalIdentityNumber = patient.NationalIdentityNumber,
-          IsRegistered = patient.IsRegistered
+            Id = patient.Id,
+            FirstName = patient.FirstName,
+            LastName = patient.LastName,
+            Phone = patient.Phone,
+            Email = patient.Email,
+            DateOfBirth = patient.DateOfBirth,
+            NationalIdentityNumber = patient.NationalIdentityNumber,
+            IsRegistered = patient.IsRegistered
+        };
+    }
+
+    public static Patient ToPatient(this PatientDTO dto)
+    {
+        return new Patient
+        {
+            Id = dto.Id,
+            FirstName = dto.FirstName,
+            LastName = dto.LastName,
+            Phone = dto.Phone,
+            Email = dto.Email,
+            DateOfBirth = dto.DateOfBirth,
+            NationalIdentityNumber = dto.NationalIdentityNumber,
+            IsRegistered = dto.IsRegistered
+        };
+    }
+
+    public static PatientWithDetailsDTO ToPatientWithDetailsDTO(this Patient patient)
+    {
+        return new PatientWithDetailsDTO
+        {
+            Id = patient.Id,
+            FirstName = patient.FirstName,
+            LastName = patient.LastName,
+            Phone = patient.Phone,
+            Email = patient.Email,
+            DateOfBirth = patient.DateOfBirth,
+            NationalIdentityNumber = patient.NationalIdentityNumber,
+            IsRegistered = patient.IsRegistered,
+            Appointments = patient.Appointments?.Select(appointment => appointment.ToAppointmentDTO())
         };
     }
 }
