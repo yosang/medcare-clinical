3d99ccf59dee264687cc3223a159ebd9fe251280
Yosmel Chiang <yosmelchiang@gmail.com>
2026-05-18 23:14:57 +0200

    fix: comment out login until implementation to make ci pass

--- Frontend/src/components/forms/LoginForm.tsx
+++ Frontend/src/components/forms/LoginForm.tsx
@@ -1,49 +1,49 @@
-import { type ChangeEvent, type SyntheticEvent, useState } from "react";
-import { useNavigate } from "react-router";
-import type { LoginState } from "../../types/Auth";
+// import { type ChangeEvent, type SyntheticEvent, useState } from "react";
+// import { useNavigate } from "react-router";
+// import type { LoginState } from "../../types/Auth";
 
-export default function Login() {
-    const navigate = useNavigate();
-    const [error, setError] = useState("");
-    const [form, setForm] = useState<LoginState>({
-        username: "",
-        password: ""
-    })
+// export default function Login() {
+//     const navigate = useNavigate();
+//     const [error, setError] = useState("");
+//     const [form, setForm] = useState<LoginState>({
+//         username: "",
+//         password: ""
+//     })
     
-    const handleSubmit = async (e: SyntheticEvent<HTMLFormElement>) => {
-        e.preventDefault();
+//     const handleSubmit = async (e: SyntheticEvent<HTMLFormElement>) => {
+//         e.preventDefault();
         
-        console.log(form)
+//         console.log(form)
         
-        // if(result.success) {
-        //     navigate("/")
-        // } else {
-        //     setError("Login failed")
-        // }
-    }
+//         // if(result.success) {
+//         //     navigate("/")
+//         // } else {
+//         //     setError("Login failed")
+//         // }
+//     }
 
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target
-        setForm(prev => ({...prev, [name]:value}))
-    }
+//     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+//         const { name, value } = e.target
+//         setForm(prev => ({...prev, [name]:value}))
+//     }
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label>Username <input 
-                    name="username"
-                    value={form.username}
-                    onChange={handleChange}
-                    /></label>
+//     return (
+//         <div>
+//             <form onSubmit={handleSubmit}>
+//                 <label>Username <input 
+//                     name="username"
+//                     value={form.username}
+//                     onChange={handleChange}
+//                     /></label>
 
-                <label>Password <input 
-                    name="password"
-                    value={form.password}
-                    onChange={handleChange}
-                /></label>
-                <button type="submit">Login</button>
-            </form>
-            {error && <p>{error}</p>}
-        </div>
-    )
-}
+//                 <label>Password <input 
+//                     name="password"
+//                     value={form.password}
+//                     onChange={handleChange}
+//                 /></label>
+//                 <button type="submit">Login</button>
+//             </form>
+//             {error && <p>{error}</p>}
+//         </div>
+//     )
+// }
