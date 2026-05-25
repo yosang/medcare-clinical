058c0f2afcac94ef7a7c713d818fa8fd76c21e5a
Yosmel Chiang <yosmelchiang@gmail.com>
2026-05-25 13:16:03 +0200

    chore: remove patient drawer from nav

--- Frontend/src/components/layout/Header.tsx
+++ Frontend/src/components/layout/Header.tsx
@@ -1,14 +1,10 @@
 import { Link } from "react-router";
 import styles from "./Header.module.css";
 import logo from "../../assets/logo.png"
-import { LogOut, User } from "lucide-react";
+import { LogOut } from "lucide-react";
 import { useLoginStore } from "../../stores/useLoginStore";
 import { useNavigate } from "react-router";
 import { toast } from "sonner";
-import { useEffect, useState, type ChangeEvent } from "react";
-import { Drawer } from "../elements/Drawer";
-import { usePatientStore } from "../../stores/usePatientStore";
-import LoadingSpinner from "./LoadingSpinner";
 import Button from "../elements/Button";
 import ThemeSwitch from "../elements/ThemSwitch";
 
@@ -16,23 +12,6 @@ export default function Header() {
 
     const navigate = useNavigate();
     const { token, logout } = useLoginStore();
-    const [open, setOpen] = useState(false);
-    const { loading, patient, getPatient } = usePatientStore();
-
-    const handleSubmit = (e: ChangeEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        const formData = new FormData(e.currentTarget)
-
-        // console.log(formData)
-
-        // To be implemented...
-
-        return
-    }
-
-    const handleDelete = () => {
-        return
-    }
 
     const handleLogout = () => {
         navigate("/");
@@ -40,79 +19,16 @@ export default function Header() {
         toast.success("Logged out!")
     }
 
-    const handleDrawerClose = () => {
-        setOpen(false);
-    }
-
-    useEffect(() => {
-        if(token) getPatient(token);
-        
-    }, [token, getPatient])
-
-    return  <>
-            <Drawer title="My Profile" isOpen={open} onClose={handleDrawerClose}>
-                {loading ? <LoadingSpinner />:(
-                    <>
-                    <form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column", height: "85vh"}}>
-                    <div style={{ display: "flex", flexDirection: "column", flex: 1 }}>
-                        <label style={{ display: "flex", flexDirection:"column", gap: "5px", padding:"var(--spacing-sm)" }}>
-                            Firstname
-                            <input 
-                                name="firstName"
-                                value={patient?.firstName}
-                                readOnly
-                                />
-                        </label>
-                        <label style={{ display: "flex", flexDirection:"column", gap: "5px", padding:"var(--spacing-sm)" }}>
-                            Lastname
-                            <input 
-                                name="lastName"
-                                value={patient?.lastName}
-                                readOnly
-                                />
-                        </label>
-                        <label style={{ display: "flex", flexDirection:"column", gap: "5px", padding:"var(--spacing-sm)" }}>
-                            Phone
-                            <input 
-                                name="phone"
-                                value={patient?.phone}
-                                readOnly
-                                />
-                        </label>
-                        <label style={{ display: "flex", flexDirection:"column", gap: "5px", padding:"var(--spacing-sm)" }}>
-                            Email
-                            <input 
-                                name="email"
-                                value={patient?.email}
-                                readOnly
-                                />
-                        </label>
-                        <label style={{ display: "flex", flexDirection:"column", gap: "5px", padding:"var(--spacing-sm)" }}>
-                            National Identity Number
-                            <input 
-                                name="nationalIdentityNumber"
-                                value={patient?.nationalIdentityNumber}
-                                readOnly
-                                />
-                        </label>
-                    </div>
-                        <div style={{ display: "flex", justifyContent: "center", padding: "var(--spacing-md) 0"}}>
-                            <Button type="submit">Upate profile</Button>
-                        </div>
-                    </form>
-                <div style={{ display: "flex", justifyContent: "center" }}>
-                    <Button onClick={handleDelete} style={{ backgroundColor: "red" }}>Delete my data</Button>
-                </div>
-                    </>
-                )}
-            </Drawer>
-            <nav className={styles.layout}>
-                <div className={styles.navLayout}>
+    return <nav>
+            <div className={styles.navLayout}>
                     <div className={styles.menuItems}>
                         <div className={styles.logo}>
                             <Link to="/"><img src={logo} alt="Web logo" style={{ height: "50px", width: "40px"}}/></Link>
                             <Link to="/"><h2 style={{ color: "var(--color-primary"}}>MedCare Clinical</h2></Link>
                         </div>
+                    </div>
+
+                    <div className={styles.menuItems}>
                         <Link to="/book">Booking</Link>
                         <Link to="/search">Search</Link>
                     </div>
@@ -122,8 +38,7 @@ export default function Header() {
                         <ThemeSwitch />
                         {token ? (
                             <>
-                            <User className={styles.navIcon} onClick={() => setOpen(true)}/>
-                            <LogOut className={styles.navIcon} onClick={handleLogout}/>
+                            <LogOut size={45} className={styles.navIcon} onClick={handleLogout}/>
                             </>
                         ):(
                         <>
@@ -134,5 +49,4 @@ export default function Header() {
                     </div>
                 </div>
             </nav>
-            </>
 }
